8be4ffc61c5b0ccc780a1017544121ace941a25b
aniruddh-jain <ani@blockseed.co>
2019-07-18 12:41:24 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Products from "./components/Product";
+import productStock from "./data.json";
+
+// Below I only need to pass the product stock because I am connecting the search bar in the product.js and that will also render it.
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Products products={productStock} />
       </div>
     );
   }
---
+++ starter-code/src/components/Product.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+
+export default class Products extends Component {
+  constructor(props) {
+    // The constructor is a method for creating and initializing an object created with a class which for me is set.state.
+    super(props); // I am passing this through super because I want to access this.props in constructor.
+
+    this.state = {
+      products: this.props.products.data,
+      search: "",
+      checkBox: false
+    };
+  }
+
+  onSearch = search => {
+    this.setState({ search });
+  };
+
+  onCheck = event => {
+    const productStock = this.state.products.filter(
+      product => product.stocked === true
+    );
+    if (!this.state.checkBox) {
+      this.setState({
+        checkBox: true,
+        products: productStock
+      });
+    } else {
+      this.setState({
+        checkBox: false,
+        products: this.props.products.data
+      });
+    }
+  };
+
+  render() {
+    const productStock = this.state.products
+      .filter(product =>
+        product.name.toLowerCase().includes(this.state.search.toLowerCase())
+      )
+      // Definelty need to use map function more, and get a deeper understanding of syntax here.
+      .map((product, index) => {
+        return (
+          <tr key={index}>
+            <td className={!product.stocked ? "red" : ""}>{product.name}</td>
+            <td>{product.price}</td>
+          </tr>
+        );
+      });
+    return (
+      <div className="container">
+        <h1>Ani's Store</h1>
+        <SearchBar onSearch={this.onSearch} onCheck={this.onCheck} />
+        <table className="table">
+          {/* Th Didnt work for me, while Thead expands it to all the space available. */}
+          <thead>
+            <tr className="bg-success text-white">
+              <th scope="col">Name</th>
+              <th scope="col">Price</th>
+            </tr>
+          </thead>
+          <tbody>{productStock}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
